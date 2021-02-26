f3dab666ee378d6490b4d7085dbd4791120c360d
clastneo.study01 <clastneo.study01@gmail.com>
2021-02-26 17:26:45 +0900

    배치파일 추가

---
+++ npm_ResetCache.bat
@@ -0,0 +1,4 @@
+cd C:\Users\admin\Documents\reactnative\med-checker
+taskkill /f /im node.exe
+
+npm run android --reset-cache
---
+++ npm_update.bat
@@ -0,0 +1,4 @@
+cd C:\Users\admin\Documents\reactnative\med-checker
+npm i -g npm-check-updates
+ncu -u
+npm install
